b272c142e41c5afdef4498b6ef486f05b4385e00
ArpitaGanatra <ganatraarpita@gmail.com>
2023-10-29 00:44:32 +0530

    feat: google authentication

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -5,14 +5,12 @@ import Footer from "./_components/footer";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 py-10">
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heros />
       </div>
-      <div>
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 };
