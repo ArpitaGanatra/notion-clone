5b80d083b6921b57d15ba50b9b5d8bb633c968ce
ArpitaGanatra <ganatraarpita@gmail.com>
2023-10-28 22:51:16 +0530

    feat: navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
 import React from "react";
+import Logo from "./Logo";
 
 const Navbar = () => {
-  return <div className=""></div>;
+  const scrolled = useScrollTop();
+  return (
+    <div
+      className={cn(
+        "flex items-center w-full p-6 z-50 bg-background fixed",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className=" flex items-center md:justify-end w-full md:ml-auto justify-between gap-x-2">
+        Login
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- hooks/use-scroll-top.tsx
+++ hooks/use-scroll-top.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useScrollTop = (threshold = 10) => {
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.screenY > threshold) {
+      if (window.scrollY > threshold) {
         setScrolled(true);
       } else {
         setScrolled(false);
